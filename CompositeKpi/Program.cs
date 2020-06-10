0d1548849d398dfe7135a838513686531d9e5e1e
Marcel <dolly.marcel@gmail.com>
2020-06-10 00:44:02 -0300

    Add CompositeKpi

---
+++ CompositeKpi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CompositeKpi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Constant : IValue
+    {
+        public int Value { get; }
+        public Constant(int value) => Value = value;
+    }
+    public interface IValue { }
+
+    public interface IValueProvider { IValue Evaluate(); }
+
+    public class ConstantValueProvider : IValueProvider
+    {
+        public IValue Evaluate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
